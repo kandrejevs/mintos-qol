6a9af948226c732e1d3f8655d240622913ddb27e
kriss <mail@kandrejevs.com>
2019-05-13 22:48:57 +0300

    Include migration in startup

--- config/php/start.sh
+++ config/php/start.sh
@@ -5,11 +5,13 @@ cd /var/www/html
 if [[ $APP_ENV = "local" ]]; then
     echo "local environment"
     composer install
+    php artisan migrate
 fi
 
 if [[ $APP_ENV = "production" ]]; then
     echo "local environment"
     composer install --optimize-autoloader --no-dev
+    php artisan migrate --force
     php artisan config:cache
     php artisan route:cache
 fi
